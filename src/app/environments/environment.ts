2ebca22c458b9f3587df2460a09e9a69686c4f49
omarmuhamed <omarmuhammedd@gmail.com>
2025-08-02 03:06:24 +0300

    Update environment.ts

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:5000/api/v1' // أو الرابط المحلي بتاعك
+  production: true,
+  apiUrl: 'https://ecommerce-api-0lbj.onrender.com/api/v1'
 };
